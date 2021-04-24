8d63dc8d0cc77fa30beb9d4bfb8eb9c0da20ba46
Just4test <Just4test@users.noreply.github.com>
2021-04-24 18:39:05 +0800

    1

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ mvn -B package
 mv target/mvc-tests-1.0-SNAPSHOT.jar ./app.jar
 
 if [ -z "$TARGET_IMAGE" ]; then
-	TARGET_IMAGE=spring-boot-testing-strategies:`git rev-parse --short HEAD`
+	TARGET_IMAGE=spring-boot-testing-strategies
 fi
 docker build -t $TARGET_IMAGE .
