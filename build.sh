5eac6c1268497c5ab69b24dab268935e0540c0fa
Just4test <Just4test@users.noreply.github.com>
2021-04-24 19:53:56 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-set -e
+set -xe
 
 mvn -B package
 mv target/mvc-tests-1.0-SNAPSHOT.jar ./app.jar
 
 if [ -z "$TARGET_IMAGE_NAME" ]; then
-	TARGET_IMAGE=target-image
+	TARGET_IMAGE_NAME=target-image
 fi
 TARGET_IMAGE_TAG=`git rev-parse --short HEAD`
 docker build -t $TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG .
 docker tag $TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG $TARGET_IMAGE_NAME:latest
 
-if [ "$CODEBUILD_CI" == "true" ]; then
+if [ "$PUSH_TO_ECR" == "true" ]; then
 	TARGET_IMAGE_REG=`aws sts get-caller-identity --query 'Account' --output text`.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
 	aws ecr get-login-password | docker login --username AWS --password-stdin $TARGET_IMAGE_REG
 	docker tag $TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG $TARGET_IMAGE_REG/$TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG
