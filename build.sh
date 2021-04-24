633c74144318ede312517e8f01b9fc155a2fdae4
Just4test <Just4test@users.noreply.github.com>
2021-04-24 19:01:16 +0800

    1

--- build.sh
+++ build.sh
@@ -4,7 +4,18 @@ set -e
 mvn -B package
 mv target/mvc-tests-1.0-SNAPSHOT.jar ./app.jar
 
-if [ -z "$TARGET_IMAGE" ]; then
-	TARGET_IMAGE=spring-boot-testing-strategies
+if [ -z "$TARGET_IMAGE_NAME" ]; then
+	TARGET_IMAGE=target-image
 fi
-docker build -t $TARGET_IMAGE .
+TARGET_IMAGE_TAG=`git rev-parse --short HEAD`
+docker build -t $TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG .
+docker tag $TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG $TARGET_IMAGE_NAME:latest
+
+if [ "$CODEBUILD_CI" == "true" ]; then
+	TARGET_IMAGE_REG=`aws sts get-caller-identity --query 'Account' --output text`.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com
+	aws ecr get-login-password | docker login --username AWS --password-stdin $TARGET_IMAGE_REG
+	docker tag $TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG $TARGET_IMAGE_REG/$TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG
+	docker tag $TARGET_IMAGE_NAME:latest $TARGET_IMAGE_REG/$TARGET_IMAGE_NAME:latest
+	docker push $TARGET_IMAGE_REG/$TARGET_IMAGE_NAME:$TARGET_IMAGE_TAG
+	docker push $TARGET_IMAGE_REG/$TARGET_IMAGE_NAME:latest
+fi
